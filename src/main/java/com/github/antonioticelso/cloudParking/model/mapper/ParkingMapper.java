c3c5a2faa1204261574b3961f3f9a4d935ef89a8
Antonio <antonio.ti.celso@gmail.com>
2022-10-21 09:11:44 -0300

    ParkingMapper - Criação de ParkingMapper

---
+++ src/main/java/com/github/antonioticelso/cloudParking/model/mapper/ParkingMapper.java
@@ -0,0 +1,24 @@
+package com.github.antonioticelso.cloudParking.model.mapper;
+
+import com.github.antonioticelso.cloudParking.model.Parking;
+import com.github.antonioticelso.cloudParking.model.dto.ParkingDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ParkingMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public ParkingDTO parkingDTO(Parking parking) {
+        return MODEL_MAPPER.map(parking, ParkingDTO.class);
+    }
+
+    public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList) {
+        return parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
+    }
+
+}
