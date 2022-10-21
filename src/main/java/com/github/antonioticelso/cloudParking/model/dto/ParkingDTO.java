72fc59aa677105075fb42f53d7a03c16632c3b00
Antonio <antonio.ti.celso@gmail.com>
2022-10-21 09:11:20 -0300

    ParkingDTO - Criação de ParkingDTO

---
+++ src/main/java/com/github/antonioticelso/cloudParking/model/dto/ParkingDTO.java
@@ -0,0 +1,23 @@
+package com.github.antonioticelso.cloudParking.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ParkingDTO {
+
+    private String id;
+    private String license;
+    private String state;
+    private String model;
+    private String color;
+    private LocalDateTime entryDate;
+    private LocalDateTime exitDate;
+    private Double bill;
+
+}
