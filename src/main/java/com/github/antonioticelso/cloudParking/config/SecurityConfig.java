5ee3ceb9bee39a82bdd9a70ef074dee2df39dc6f
Antonio <antonio.ti.celso@gmail.com>
2022-10-25 18:45:21 -0300

    Spring SecurityConfig - Implemetando

---
+++ src/main/java/com/github/antonioticelso/cloudParking/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.github.antonioticelso.cloudParking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("ddd@555"))
+                .roles("USER")
+                .and()
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
