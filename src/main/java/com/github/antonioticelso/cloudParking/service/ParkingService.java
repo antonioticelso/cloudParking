f0d894878ec8154af6eaa932a1e1eee6a82271c3
Antonio <antonio.ti.celso@gmail.com>
2022-10-25 11:08:58 -0300

    Calculo do valor de saída

--- src/main/java/com/github/antonioticelso/cloudParking/service/ParkingService.java
+++ src/main/java/com/github/antonioticelso/cloudParking/service/ParkingService.java
@@ -5,6 +5,8 @@ import com.github.antonioticelso.cloudParking.model.Parking;
 import com.github.antonioticelso.cloudParking.model.ParkingCheckOut;
 import com.github.antonioticelso.cloudParking.repository.ParkingRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.*;
@@ -20,6 +22,7 @@ public class ParkingService {
         this.repository = repository;
     }
 
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public List<Parking> findAll() {
         return repository.findAll();
     }
@@ -28,11 +31,13 @@ public class ParkingService {
         return UUID.randomUUID().toString().replace("-", "");
     }
 
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public Parking findById(String id) {
         return repository.findById(id).orElseThrow(() -> new ParkingNotFoundException(id));
 
     }
 
+    @Transactional
     public Parking create(Parking parkingCreate) {
         String id = getUUID();
         parkingCreate.setId(id);
@@ -43,12 +48,14 @@ public class ParkingService {
 
     }
 
+    @Transactional
     public void delete(String id) {
         findById(id);
         repository.deleteById(id);
 
     }
 
+    @Transactional
     public Parking update(String id, Parking parkingCreate) {
         Parking parking = findById(id);
         parking.setColor(parkingCreate.getColor());
@@ -62,6 +69,7 @@ public class ParkingService {
 
     }
 
+    @Transactional
     public Parking checkOut(String id) {
         Parking parking = findById(id);
         parking.setExitDate(LocalDateTime.now());
